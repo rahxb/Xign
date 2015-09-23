85748025fdabd0ded16311bd1c2cdd9b479490f1
rahxb <rahxendev@gmail.com>
2015-09-23 20:59:20 +0900

    とりあえずコミット

--- Xign/Xign/frmFileManager.cs
+++ Xign/Xign/frmFileManager.cs
@@ -12,10 +12,35 @@ namespace Xign
 {
     public partial class frmFileManager : Form
     {
+        class KeyAndDisplayName
+        {
+            public string Key { get; set; }
+            public UInt64 Location { get; set; }
+        }
+        List<string> _KeyList = new List<string>();
+
         public frmFileManager()
         {
             InitializeComponent();
         }
 
+        private void frmFileManager_Load(object sender, EventArgs e)
+        {
+            trFolder.LabelEdit = true;
+            trFolder.Nodes.Add("root", "root");
+        }
+
+        private void mnuFRBNewFolder_Click(object sender, EventArgs e)
+        {
+            string strKey = Guid.NewGuid().ToString();
+            string strDisplayName = "新規フォルダ" + Environment.TickCount;
+
+            _KeyList.Add(strKey);
+
+            TreeNode tn = trFolder.SelectedNode.Nodes.Add(strKey, strDisplayName);
+            trFolder.SelectedNode.ExpandAll();
+            trFolder.SelectedNode = tn;
+            tn.BeginEdit();
+        }
     }
 }
