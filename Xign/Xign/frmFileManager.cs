685d945703cad2f480d03ea03e6a1f3021cbb707
rahxb <rahxendev@gmail.com>
2015-09-24 23:29:17 +0900

    本日はここまで ('◇')ゞ

--- Xign/Xign/frmFileManager.cs
+++ Xign/Xign/frmFileManager.cs
@@ -13,6 +13,9 @@ namespace Xign
 {
     public partial class frmFileManager : Form
     {
+
+        #region Class, Enum, Const
+
         /*
 
         [guid.csv]
@@ -47,26 +50,46 @@ namespace Xign
         ...
         */
 
-        const string DATA_FILE = "data.dat";
 
-        public enum ModuleKind
+        public enum GuidModuleKind
         {
-            Unused=0x55,   // U 
-            File =0x46,    // F
-            Folder = 0x4f, // O
+            None = 0x00,    // None
+            Unused  = 0x55, // U 
+            File    = 0x46, // F
+            Folder  = 0x4f, // O
 
             // ... UFO!?
         }
 
-        public class ModuleItem
+        public class GuidModuleItem
         {
-            public string GUID { get; set; }
-            public ModuleKind Kind { get; set; }
-            public UInt64 Location { get; set; }
-            public UInt64 Length { get; set; }
-            public string Hash { get; set; }
+            public string guid { get; set; } = "";
+            public GuidModuleKind kind { get; set; } = GuidModuleKind.None;
+            public UInt64 location { get; set; } = 0;
+            public UInt64 length { get; set; } = 0;
+            public string hash { get; set; } = "";
         }
-        List<ModuleItem> _guidList = new List<ModuleItem>();
+
+
+        public class DataModuleItem
+        {
+            public string guid { get; set; } = "";
+            public string parent_guid { get; set; } = "";
+            public string f_name { get; set; } = "";
+            public string content { get; set; } = "";
+        }
+
+        const string GUID_FILE = "guid.csv";
+        const string DATA_FILE = "data.dat";
+
+        #endregion
+
+        List<GuidModuleItem> _guidList = new List<GuidModuleItem>();
+
+
+
+
+        #region GUI
 
         public frmFileManager()
         {
@@ -78,37 +101,240 @@ namespace Xign
             trFolder.LabelEdit = true;
             trFolder.Nodes.Add("root", "root");
             trFolder.AfterLabelEdit += TrFolder_AfterLabelEdit;
+
+            LoadGuid();
+
+            foreach (GuidModuleItem gmi in _guidList)
+            {
+                DataModuleItem dmi = LLoadData(gmi.guid);
+                if (dmi.parent_guid.Equals(""))
+                    trFolder.Nodes["root"].Nodes.Add(dmi.guid, dmi.f_name);
+            }
+            trFolder.ExpandAll();
         }
 
+        /// <summary>
+        /// ツリーのラベルの編集が完了したときのイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TrFolder_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
         {
-            LAddFolder(e.Label);
+            LAddFolder(e.Node.Text, "");
         }
 
-        void LAddFolder(string strFolderName)
+        private void mnuFRBNewFolder_Click(object sender, EventArgs e)
+        {
+            string strKey = Guid.NewGuid().ToString();
+            string strDummyName = "新規フォルダ" + Environment.TickCount;
+
+            //GuidModuleItem gmi = new GuidModuleItem();
+            //gmi.guid = Guid.NewGuid().ToString();
+            //gmi.kind = GuidModuleKind.Folder;
+
+            //_guidList.Add(gmi);
+
+            TreeNode tn = trFolder.SelectedNode.Nodes.Add(strKey, strDummyName);
+            trFolder.SelectedNode.ExpandAll();
+            trFolder.SelectedNode = tn;
+            tn.BeginEdit();
+        }
+
+        #endregion
+
+
+
+
+
+
+
+        #region Functions
+
+        /// <summary>
+        /// ツリーにフォルダを追加する
+        /// </summary>
+        /// <param name="strFolderName"></param>
+        void LAddFolder(string strFolderName, string parent_guid)
         {
             // データ追加
-            ModuleItem mi;
-            using (FileStream fs = new FileStream(DATA_FILE, FileMode.Create, FileAccess.ReadWrite))
+            GuidModuleItem gmi;
+            using (FileStream fs = new FileStream(DATA_FILE, FileMode.OpenOrCreate, FileAccess.ReadWrite))
             {
-                mi = AddData(fs, Guid.NewGuid().ToString(), "", strFolderName, new byte[] { });
+                gmi = AddData(fs, Guid.NewGuid().ToString(), parent_guid, strFolderName, new byte[] { });
             }
 
             // ヘッダ追加
-            // mi
+            _guidList.Add(gmi);
+
+            // guidをファイルに保存する
+            SaveGuid();
+        }
+
+        void LoadGuid()
+        {
+            _guidList.Clear();
+
+            bool bFirst = true;
+            if (System.IO.File.Exists(GUID_FILE) == false)
+                return;
+
+            string[] arLines = System.IO.File.ReadAllLines(GUID_FILE);
+            string[] arHeader = null;
+            foreach(string strLine in arLines)
+            {
+                string[] arFields = strLine.Split('\t');
+                if (bFirst)
+                {
+                    bFirst = false;
+                    arHeader = arFields;
+                }
+                else
+                {
+                    GuidModuleItem gmi = new GuidModuleItem();
+                    for (int i = 0; i < arHeader.Length; i++)
+                    {
+                        string name = arHeader[i];
+                        string value = arFields[i];
+                        switch(name.ToUpper())
+                        {
+                            case "GUID":
+                                gmi.guid = value;
+                                break;
+                            case "KIND":
+                                gmi.kind = ToModuleKind(value.ToUpper());
+                                break;
+                            case "LOCATION":
+                                gmi.location = UInt64.Parse(value);
+                                break;
+                            case "LENGTH":
+                                gmi.length = UInt64.Parse(value);
+                                break;
+                            case "HASH":
+                                gmi.hash = value;
+                                break;
+                        }
+                    }
+                    _guidList.Add(gmi);
+                }
+            }
+        }
+
+        GuidModuleKind ToModuleKind(string str)
+        {
+            GuidModuleKind mk = GuidModuleKind.None;
+            switch (str.ToUpper())
+            {
+                case "U":
+                    mk = GuidModuleKind.Unused;
+                    break;
+                case "F":
+                    mk = GuidModuleKind.File;
+                    break;
+                case "O":
+                    mk = GuidModuleKind.Folder;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            return mk;
+        }
+
+        void SaveGuid()
+        {
+            /*
+            [guid.csv]
+            guid	kind	location	length  hash
+            00-110.	(U/F/O)	0x0001		0x0200  (sha256)
+            */
+
+            List<string> arContent = new List<string>();
+
+            arContent.Add(string.Join("\t", new string[] { "guid", "kind", "location", "length", "hash" }));
+            foreach(GuidModuleItem gmi in _guidList)
+            {
+                arContent.Add(string.Join("\t", new string[] { gmi.guid, char.ConvertFromUtf32((int)gmi.kind), gmi.location.ToString(), gmi.length.ToString(), gmi.hash }));
+            }
+            System.IO.File.WriteAllText(GUID_FILE, string.Join("\r\n", arContent.ToArray()));
         }
 
         void LAddFile(string strFilePath, string parent_guid)
         {
             string name = System.IO.Path.GetFileName(strFilePath);
             byte[] binary = System.IO.File.ReadAllBytes(strFilePath);
-            using (FileStream fs = new FileStream(DATA_FILE, FileMode.Create, FileAccess.ReadWrite))
+            using (FileStream fs = new FileStream(DATA_FILE, FileMode.OpenOrCreate, FileAccess.ReadWrite))
             {
                 AddData(fs, "", parent_guid, name, binary);
             }
         }
 
-        public ModuleItem AddData(Stream srm, string guid, string parent_guid, string f_name, byte[] file_data)
+        DataModuleItem LLoadData(string guid)
+        {
+            GuidModuleItem gmiFound = null;
+            foreach (GuidModuleItem gmi in _guidList)
+            {
+                if (gmi.guid.Equals(guid))
+                {
+                    gmiFound = gmi;
+                    break;
+                }
+            }
+            if (gmiFound == null)
+                return null;
+
+            byte[] binData = null;
+            using (FileStream fs = new FileStream(DATA_FILE, FileMode.Open, FileAccess.Read))
+            {
+                fs.Position = (long)gmiFound.location;
+                binData = new byte[gmiFound.length];
+                fs.Read(binData, 0, binData.Length);
+            }
+            string strData = System.Text.Encoding.UTF8.GetString(binData);
+            string[] arUL = strData.Split(new string[] { "\r\n\r\n" }, 2, StringSplitOptions.None);
+            string strUpper = arUL[0];
+
+            string strLower = "";
+            if (2 <= arUL.Length)
+                strLower = arUL[1];
+
+            string[] arLines = strUpper.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            DataModuleItem dmi = new DataModuleItem();
+            foreach(string strLine in arLines)
+            {
+                string[] arFields = strLine.Split(new char[] { ':' }, 2);
+                if (2 <= arFields.Length)
+                {
+                    string name = arFields[0];
+                    string value = arFields[1];
+                    switch (name.ToUpper())
+                    {
+                        case "GUID":
+                            dmi.guid = value;
+                            break;
+                        case "PARENT-GUID":
+                            dmi.parent_guid = value;
+                            break;
+                        case "F-NAME":
+                            dmi.f_name = Base64DecodeText(value);
+                            break;
+                    }
+                }
+            }
+            dmi.content = strLower;
+            return dmi;
+        }
+
+        string Base64DecodeText(string strBase64EncodeAndUTF8)
+        {
+            return System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(strBase64EncodeAndUTF8));
+        }
+
+        string Base64EncodeText(string strPlain)
+        {
+            return System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(strPlain));
+        }
+
+
+        public GuidModuleItem AddData(Stream srm, string guid, string parent_guid, string f_name, byte[] file_data)
         {
             /*
             guid:(ascii) (crlf)
@@ -127,8 +353,8 @@ namespace Xign
             srm.Position = srm.Length;
             long lngLocation = srm.Position;
 
-            string b64f_name = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(f_name));
-            ModuleKind mkKind = ModuleKind.File;
+            string b64f_name = Base64EncodeText(f_name);
+            GuidModuleKind mkKind = GuidModuleKind.File;
 
             // 追記する内容を準備する
             StringBuilder sbText = new StringBuilder();
@@ -138,12 +364,12 @@ namespace Xign
             sbText.Append("" + "\r\n");
             if (file_data.Length == 0)
             {
-                mkKind = ModuleKind.Folder;
+                mkKind = GuidModuleKind.Folder;
                 sbText.Append("" + "\r\n");
             }
             else
             {
-                mkKind = ModuleKind.File;
+                mkKind = GuidModuleKind.File;
                 Convert.ToBase64String(file_data, Base64FormattingOptions.InsertLineBreaks);
             }
             byte[] binary = System.Text.Encoding.UTF8.GetBytes(sbText.ToString());
@@ -153,31 +379,19 @@ namespace Xign
             srm.WriteByte(0x00);
 
             // ヘッダを準備
-            ModuleItem miResult = new ModuleItem();
-            miResult.GUID = guid;
-            miResult.Hash = "";
-            miResult.Kind = mkKind;
-            miResult.Length = (UInt64)binary.Length;
-            miResult.Location = (UInt64)lngLocation;
+            GuidModuleItem gmiResult = new GuidModuleItem();
+            gmiResult.guid = guid;
+            gmiResult.hash = "";
+            gmiResult.kind = mkKind;
+            gmiResult.length = (UInt64)binary.Length;
+            gmiResult.location = (UInt64)lngLocation;
 
-            return miResult;
+            return gmiResult;
         }
 
-        private void mnuFRBNewFolder_Click(object sender, EventArgs e)
-        {
-            string strKey = Guid.NewGuid().ToString();
-            string strDummyName = "新規フォルダ" + Environment.TickCount;
+        #endregion
 
-            ModuleItem mi = new ModuleItem();
-            mi.GUID = Guid.NewGuid().ToString();
-            mi.Kind = ModuleKind.Folder;
-            
-            _guidList.Add(mi);
 
-            TreeNode tn = trFolder.SelectedNode.Nodes.Add(strKey, strDummyName);
-            trFolder.SelectedNode.ExpandAll();
-            trFolder.SelectedNode = tn;
-            tn.BeginEdit();
-        }
+
     }
 }
